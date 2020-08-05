aaac34a7ebd991714aafb287d354d7e68f5877ef
Mike <604259003@webmail.npru.ac.th>
2020-08-05 16:02:08 +0700

    Updates

---
+++ precache-manifest.174dc70161bf9b5ebf5ef74467ed1442.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37141413eef59cb8839c36b013cdf421",
+    "url": "/react-project-mobile/index.html"
+  },
+  {
+    "revision": "98c72e0c6424199c9c6f",
+    "url": "/react-project-mobile/static/css/main.329e6fc9.chunk.css"
+  },
+  {
+    "revision": "6875c71f038d49f6af60",
+    "url": "/react-project-mobile/static/js/2.3abdd5e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-project-mobile/static/js/2.3abdd5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c72e0c6424199c9c6f",
+    "url": "/react-project-mobile/static/js/main.3173e840.chunk.js"
+  },
+  {
+    "revision": "046442c4921b290cd955",
+    "url": "/react-project-mobile/static/js/runtime-main.41c7454f.js"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/react-project-mobile/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/react-project-mobile/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/react-project-mobile/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/react-project-mobile/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
